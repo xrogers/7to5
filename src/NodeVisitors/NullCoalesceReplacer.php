7b3e0f4254aadd81cf1a7ef2ddad68d5fcdadcc1
Daniil Gentili <daniil@daniil.it>
2018-12-23 15:00:14 +0100

    Error suppressor parsing

--- src/NodeVisitors/NullCoalesceReplacer.php
+++ src/NodeVisitors/NullCoalesceReplacer.php
@@ -16,14 +16,18 @@ class NullCoalesceReplacer extends NodeVisitorAbstract
         if (!$node instanceof Coalesce) {
             return;
         }
+        $test = $node->left;
+        if ($test instanceof Node\Expr\ErrorSuppress) {
+            $test = $test->expr;
+        }
         switch(true)
         {
-            case $node->left instanceof Node\Expr\FuncCall:
-            case $node->left instanceof Node\Expr\MethodCall:
-            case $node->left instanceof Node\Expr\StaticCall:
+            case $test instanceof Node\Expr\FuncCall:
+            case $test instanceof Node\Expr\MethodCall:
+            case $test instanceof Node\Expr\StaticCall:
                 $notEmptyCall = new Node\Expr\BooleanNot(new Node\Expr\FuncCall(new Node\Name('empty'), [$node->left]));
                 return new Node\Expr\Ternary($notEmptyCall, $node->left, $node->right);
-            case $node->left instanceof Node\Expr\BinaryOp:
+            case $test instanceof Node\Expr\BinaryOp:
                 $issetCall = new Node\Expr\FuncCall(new Node\Name('isset'), [$node->left->right]);
                 $node->left->right = new Node\Expr\Ternary($issetCall, $node->left->right, $node->right);
                 return $node->left;
